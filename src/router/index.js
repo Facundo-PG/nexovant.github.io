a5346134dac6e9a1e96c45de27d4a2e945bbee5f
facundo.ramirez <facundo.ramirez@pgtech.ar>
2025-11-20 22:53:41 -0300

    fix in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LandingView from '../views/LandingView.vue';
-import HomeView from '../views/HomeView.vue';
+import LandingView from '@../views/LandingView.vue';
+import HomeView from '@../views/HomeView.vue';
 
 // 1. Definimos las rutas
 const routes = [
