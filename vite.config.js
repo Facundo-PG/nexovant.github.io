e2339d99ac259c9cc2c8276c8cc409a65488eaac
facundo.ramirez <facundo.ramirez@pgtech.ar>
2025-11-21 03:20:06 -0300

    push

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/nexovant/', 
+  base: './', 
   // 2. Activar el plugin de Vue
   plugins: [vue()], 
 
