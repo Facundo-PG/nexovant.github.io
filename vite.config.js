41c66992a37cbc771905b94b4ff30f6b30ba9229
facundo.ramirez <facundo.ramirez@pgtech.ar>
2025-11-21 02:22:06 -0300

    feature

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 // Si usas React, necesitas este import
 // import react from '@vitejs/plugin-react'; 
 
 const REPO_NAME = '/nexovant/'; 
 
 export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   base: REPO_NAME, 
   // plugins: [react()], // Descomenta si usas React
-  
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
   build: {
     // Definimos el directorio de salida (ya está correcto)
     outDir: 'dist', 
